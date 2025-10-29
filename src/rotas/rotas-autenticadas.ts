f6f73d758d894d6b2ce07ed149a00ecc5512c84c
tyler the coder <teylor.aquino@estudante.ifms.edu.br>
2025-10-29 10:45:18 -0400

    adcionaria usuario Adm

--- src/rotas/rotas-autenticadas.ts
+++ src/rotas/rotas-autenticadas.ts
@@ -2,6 +2,7 @@ import {Router} from 'express'
 
 import carrinhoController from '../carrinho/carrinho.controller.js'
 import produtosController from '../produtos/produtos.controller.js'
+import usuariosControllerAdm from '../usuarios/usuarios.controllerAdm.js'
 
 const rotas = Router()
 rotas.post('/produtos',produtosController.adicionar)
@@ -11,5 +12,8 @@ rotas.post('/adicionarItem',carrinhoController.adicionarItem)
 rotas.post('/removerItem',carrinhoController.removerItem)
 rotas.get('/carrinho/:usuarioId',carrinhoController.listar)
 rotas.delete('/carrinho/:usuarioId',carrinhoController.remover)
+
+// Rotas de administração
+rotas.post('/adicionarUsuarioAdm', usuariosControllerAdm.adicionar)
 
 export default rotas
